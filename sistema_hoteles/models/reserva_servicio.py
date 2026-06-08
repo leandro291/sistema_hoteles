7034490d2afb835f34759f93c488bbb66df51317
leandro291 <lenadrito1@gmail.com>
2026-06-08 18:35:23 -0500

    feat: Agregar modelo ReservaServicio y esquema de validación

---
+++ sistema_hoteles/models/reserva_servicio.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+from pydantic import BaseModel
+
+class ReservaServicioSchema(BaseModel):
+    cantidad: int
+    subtotal: Decimal
+
+class ReservaServicio:
+    def __init__(self, id_servicio: None, id_reserva: int, cantidad: int, subtotal: Decimal):
+        self.id_servicio = id_servicio    
+        self.id_reserva = id_reserva      
+        self.cantidad = cantidad
+        self.subtotal = Decimal
