150eb4b89eacabf806cfc7d925bc7b9a3b39f155
leandro291 <lenadrito1@gmail.com>
2026-06-08 18:36:58 -0500

    feat: Agregar importaciones de modelos en el módulo principal

---
+++ sistema_hoteles/models/__init__.py
@@ -0,0 +1,8 @@
+from .pago import Pago, PagoSchema
+from .usuario import Usuario, UsuarioSchema
+from .reserva import Reserva, ReservaSchema
+from .servicio import Servicio, ServicioSchema
+from .habitacion import Habitacion, HabitacionSchema
+from .acompanante import Acompanante, AcompananteSchema
+from .tipo_habitacion import TipoHabitacion, TipoHabitacionSchema
+from .reserva_servicio import ReservaServicio, ReservaServicioSchema
