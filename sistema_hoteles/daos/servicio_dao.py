082d951bcb4ce1fa7cdfd67862ba337d85df6517
leandro291 <lenadrito1@gmail.com>
2026-06-09 00:55:46 -0500

    feat: Refactor ServicioDAO para heredar de BaseDAO y simplificar la inserción de servicios

--- sistema_hoteles/daos/servicio_dao.py
+++ sistema_hoteles/daos/servicio_dao.py
@@ -1,37 +1,23 @@
-from config.database import ConexionDB
+from daos.base_dao import BaseDAO
 from models.servicio import Servicio
 
-class ServicioDAO:
-    def __init__(self):
-        self.db = ConexionDB()
-        self.conexion = self.db.obtener_conexion()
+class ServicioDAO(BaseDAO):
 
     def insertar_servicio(self, servicio: "Servicio") -> int:
-
-        cursor = self.conexion.cursor()
-
-        try:
             
-            consulta = """
-                INSERT INTO servicio (nombre, descripcion, precio) 
-                VALUES (%s, %s, %s)
-                RETURNING idservicio
-            """
+        consulta = """
+            INSERT INTO servicio (nombre, descripcion, precio) 
+            VALUES (%s, %s, %s)
+            RETURNING idservicio
+        """
 
-            valores = (
-                servicio.nombre,
-                servicio.descripcion,
-                servicio.precio
-            )
+        valores = (
+            servicio.nombre,
+            servicio.descripcion,
+            servicio.precio
+        )
 
-            cursor.execute(consulta, valores)
-            id_obtenido = cursor.fetchone()[0]
-            self.conexion.commit()
+        return self.insertar_y_retornar_id(consulta, valores)
 
-            return id_obtenido
 
-        except Exception as e:
-            return e
-        finally:
-            if cursor:
-                cursor.close()
+
