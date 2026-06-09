a90c97e70e78ff0f5ccc24578ce901cd453e16eb
leandro291 <lenadrito1@gmail.com>
2026-06-09 00:55:19 -0500

    feat: Refactor PagoDAO para heredar de BaseDAO y simplificar la inserción de pagos

--- sistema_hoteles/daos/pago_dao.py
+++ sistema_hoteles/daos/pago_dao.py
@@ -1,42 +1,24 @@
-from config.database import ConexionDB
+from daos.base_dao import BaseDAO
 from models.pago import Pago
 
-class PagoDAO:
-    def __init__(self):
-        self.db = ConexionDB()
-        self.conexion = self.db.obtener_conexion()
+class PagoDAO(BaseDAO):
 
     def insertar_pago(self, pago: "Pago") -> int:
 
-        cursor = self.conexion.cursor()
+        consulta = """
+            INSERT INTO pago (monto, fecha_pago, metodo_pago, estado_pago, tipo_comprobante, idreserva)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            RETURNING idpago
+        """
 
-        try:
+        valores = (
+            pago.monto,
+            pago.fecha_pago,
+            pago.metodo_pago,
+            pago.estado_pago,
+            pago.tipo_comprobante,
+            pago.id_reserva
+        )
 
-            consulta = """
-                INSERT INTO pago (monto, fecha_pago, metodo_pago, estado_pago, tipo_comprobante, idreserva)
-                VALUES (%s, %s, %s, %s, %s, %s,)
-                RETURNING idpago
-            """
-
-            valores = (
-                pago.monto,
-                pago.fecha_pago,
-                pago.metodo_pago,
-                pago.estado_pago,
-                pago.tipo_comprobante,
-                pago.id_reserva
-            )
-
-            cursor.execute(consulta, valores)
-            id_obtenido = cursor.fetchone()[0]
-            self.conexion.commit()
-
-            return id_obtenido
-
-        except Exception as e:
-            self.conexion.rollback()
-            raise e
-        finally:
-            if cursor:
-                cursor.close()
+        return self.insertar_y_retornar_id(consulta, valores)
         
