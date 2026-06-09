c582ed0499a07de76984d97cf55a311d4c753d00
leandro291 <lenadrito1@gmail.com>
2026-06-08 19:34:39 -0500

    fix: Corregir la firma del constructor de la clase ReservaServicio para incluir el tipo correcto de id_servicio y ajustar la inicialización de subtotal

--- sistema_hoteles/models/reserva_servicio.py
+++ sistema_hoteles/models/reserva_servicio.py
@@ -6,8 +6,9 @@ class ReservaServicioSchema(BaseModel):
     subtotal: Decimal
 
 class ReservaServicio:
-    def __init__(self, id_servicio: None, id_reserva: int, cantidad: int, subtotal: Decimal):
+    def __init__(self, id_servicio: int, id_reserva: int, cantidad: int, subtotal: Decimal):
         self.id_servicio = id_servicio    
         self.id_reserva = id_reserva      
         self.cantidad = cantidad
-        self.subtotal = Decimal
+        self.subtotal = subtotal
+
