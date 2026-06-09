bd7478429169cf3ce17b6e8f1d2d3070dfcfdcf9
leandro291 <lenadrito1@gmail.com>
2026-06-08 19:33:51 -0500

    feat: Agregar id_reserva al constructor de la clase Acompanante

--- sistema_hoteles/models/acompanante.py
+++ sistema_hoteles/models/acompanante.py
@@ -7,9 +7,13 @@ class AcompananteSchema(BaseModel):
     edad: int
 
 class Acompanante:
-    def __init__(self, id_acompanante: None, nombre: str, apellido: str, dni: str, edad: int):
+    def __init__(self, id_reserva: int, nombre: str, apellido: str, dni: str, edad: int, id_acompanante: None = None):
         self.id_acompanante = id_acompanante
+        self.id_reserva = id_reserva
         self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
         self.edad = edad
+
+
+
