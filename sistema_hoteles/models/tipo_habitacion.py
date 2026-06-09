b89e6ff14b598050eab4947d9976317eea95db02
leandro291 <lenadrito1@gmail.com>
2026-06-08 19:35:25 -0500

    feat: Actualizar el constructor de la clase TipoHabitacion para incluir id_tipo_habitacion como opcional

--- sistema_hoteles/models/tipo_habitacion.py
+++ sistema_hoteles/models/tipo_habitacion.py
@@ -8,11 +8,12 @@ class TipoHabitacionSchema(BaseModel):
     capacidad: int
 
 class TipoHabitacion:
-    def __init__(self, id_tipo_habitacion: None, nombre: str, precio: Decimal, capacidad: int):
+    def __init__(self, nombre: str, precio: Decimal, capacidad: int, id_tipo_habitacion: None = None):
         self.id_tipo_habitacion = id_tipo_habitacion
         self.nombre = nombre
         self.precio = precio
         self.capacidad = capacidad
+
         self.habitaciones: List[int] = []
 
 
