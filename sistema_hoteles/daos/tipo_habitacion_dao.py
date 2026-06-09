3964739eab3fb4651ebd2fd61a4d8bfd41752a51
leandro291 <lenadrito1@gmail.com>
2026-06-09 00:55:53 -0500

    feat: Refactor TipoHabitacionDAO para heredar de BaseDAO y simplificar la inserción de tipos de habitación

--- sistema_hoteles/daos/tipo_habitacion_dao.py
+++ sistema_hoteles/daos/tipo_habitacion_dao.py
@@ -1,37 +1,21 @@
-from config.database import ConexionDB
+from daos.base_dao import BaseDAO
 from models.tipo_habitacion import TipoHabitacion
 
-
-class TipoHabitacionDAO:
-    def __init__(self):
-        self.db = ConexionDB()
-        self.conexion = self.db.obtener_conexion()
+class TipoHabitacionDAO(BaseDAO):
 
     def insertar_tipo_habitacion(self, tipo_habitacion: "TipoHabitacion") -> int:
 
-        cursor = self.conexion.cursor()
+        consulta = """
+            INSERT INTO tipo_habitacion (nombre, precio, capacidad)
+            VALUES (%s, %s, %s)
+            RETURNING idtipo_habitacion
+        """
 
-        try:
+        valores = (
+            tipo_habitacion.nombre,
+            tipo_habitacion.precio,
+            tipo_habitacion.capacidad
+        )
 
-            consulta = """
-                INSERT INTO tipo_habitacion (nombre, precio, capacidad)
-                VALUES (%s, %s, %s)
-                RETURNING idtipo_habitacion
-            """
+        return self.insertar_y_retornar_id(consulta, valores)
 
-            valores = (
-                tipo_habitacion.nombre,
-                tipo_habitacion.precio,
-                tipo_habitacion.capacidad
-            )
-
-            cursor.execute(consulta, valores)
-            id_obtenido = cursor.fetchone()[0]
-            self.conexion.commit()
-
-        except Exception as e:
-            self.conexion.rollback()
-            raise e
-        finally:
-            if cursor:
-                cursor.close()
