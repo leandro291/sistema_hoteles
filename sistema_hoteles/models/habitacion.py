12a97038c239ddaf042a8ef8348e5b8df4856e19
leandro291 <lenadrito1@gmail.com>
2026-06-09 00:31:00 -0500

    fix: Corregir el nombre del atributo tipo_habitacion a id_tipo_habitacion en la clase Habitacion

--- sistema_hoteles/models/habitacion.py
+++ sistema_hoteles/models/habitacion.py
@@ -10,7 +10,7 @@ class HabitacionSchema(BaseModel):
 class Habitacion:
     def __init__(self, numero_piso: int, numero_habitacion: int, estado: str, id_tipo_habitacion: int, id_habitacion: int | None = None):
         self.id_habitacion = id_habitacion
-        self.tipo_habitacion = id_tipo_habitacion
+        self.id_tipo_habitacion = id_tipo_habitacion
         self.estado = estado
         self.numero_piso = numero_piso
         self.numero_habitacion = numero_habitacion
