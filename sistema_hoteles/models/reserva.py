e42ee468c08becac8efbcf9ad4b33aca19c226ca
leandro291 <lenadrito1@gmail.com>
2026-06-08 19:34:48 -0500

    feat: Actualizar el constructor de la clase Reserva para incluir id_cliente y ajustar la inicialización de atributos

--- sistema_hoteles/models/reserva.py
+++ sistema_hoteles/models/reserva.py
@@ -1,3 +1,4 @@
+from typing import List
 from datetime import date
 from decimal import Decimal
 from pydantic import BaseModel
@@ -11,11 +12,19 @@ class ReservaSchema(BaseModel):
     total: Decimal
 
 class Reserva:
-    def __init__(self, id_reserva: None, fecha_reserva: date, fecha_entrada: date, fecha_salida: date, cantidad_huesped: int, estado_reserva: str, total: Decimal):
+    def __init__(self, id_cliente: int, fecha_reserva: date, fecha_entrada: date, fecha_salida: date, cantidad_huesped: int, estado_reserva: str, total: Decimal, id_reserva: None=None):
         self.id_reserva = id_reserva
+        self.id_cliente = id_cliente
         self.fecha_reserva = fecha_reserva
         self.fecha_entrada = fecha_entrada
         self.fecha_salida = fecha_salida
         self.cantidad_huesped = cantidad_huesped
         self.estado_reserva = estado_reserva
         self.total = total
+
+        self.pagos: List[int] = []
+        self.acompanantes: List[int] = []
+        self.reserva_servicio: List[int] = []
+        self.reserva_habitacion: List[int] = []
+
+        
