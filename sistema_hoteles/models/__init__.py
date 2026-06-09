d05cabad9b3051b195f107f0d1201c8d4c400e00
leandro291 <lenadrito1@gmail.com>
2026-06-08 19:00:37 -0500

    feat: Agregar importaciones de Cliente y reserva_habitacion en el módulo principal

--- sistema_hoteles/models/__init__.py
+++ sistema_hoteles/models/__init__.py
@@ -1,7 +1,9 @@
 from .pago import Pago, PagoSchema
 from .usuario import Usuario, UsuarioSchema
+from .cliente import Cliente, ClienteSchema
 from .reserva import Reserva, ReservaSchema
 from .servicio import Servicio, ServicioSchema
+from .reserva_habitacion import reserva_habitacion
 from .habitacion import Habitacion, HabitacionSchema
 from .acompanante import Acompanante, AcompananteSchema
 from .tipo_habitacion import TipoHabitacion, TipoHabitacionSchema
