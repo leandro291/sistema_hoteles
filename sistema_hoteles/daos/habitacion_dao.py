378f9d3143a7d9433751cbb0bb645812f49fc7c9
leandro291 <lenadrito1@gmail.com>
2026-06-09 21:11:15 -0500

    feat: Implementar método para obtener el precio de una habitación según su ID

--- sistema_hoteles/daos/habitacion_dao.py
+++ sistema_hoteles/daos/habitacion_dao.py
@@ -20,3 +20,29 @@ class HabitacionDAO(BaseDAO):
 
         return self.insertar_y_retornar_id(consulta, valores)
     
+    def obtener_precio_por_habitacion(self, id_habitacion: int):
+
+        consulta = """
+            SELECT th.precio
+            FROM habitacion h
+            JOIN tipo_habitacion th ON h.idhabitacion = th.idhabitacion
+            WHERE h.idhabitacion = %s
+        """
+
+        valores = (id_habitacion,)
+
+        cursor = self.conexion.cursor()
+
+        try:
+            cursor.execute(consulta, valores)
+            resultado = cursor.fetchone()    
+
+            if resultado is None:
+                raise Exception("La habitacion no existe o no tiene un tipo asignado")
+            
+            return float(resultado[0])
+        
+        except Exception as e:
+            raise e
+        finally:
+            cursor.close()
