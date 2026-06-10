4359ebf9c6883add93277f8ac711c90bc0157192
leandro291 <lenadrito1@gmail.com>
2026-06-09 22:27:42 -0500

    feat: Agregar método para obtener el precio de un servicio por su ID

--- sistema_hoteles/daos/servicio_dao.py
+++ sistema_hoteles/daos/servicio_dao.py
@@ -18,6 +18,32 @@ class ServicioDAO(BaseDAO):
         )
 
         return self.insertar_y_retornar_id(consulta, valores)
+    
+    def obtener_precio_por_id(self, id_servicio: int) -> int:
+
+        consulta = """
+                SELECT precio 
+                FROM servicio
+                WHERE idservicio = %s
+            """
+        
+        valores = (id_servicio,)
+
+        cursor = self.conexion.cursor()
+
+        try:
+            cursor.execute(consulta, valores)
+            resultado = cursor.fetchone()    
+
+            if resultado is None:
+                raise Exception("El servicio no existe o no tiene un tipo asignado")
+            
+            return float(resultado[0])
+        
+        except Exception as e:
+            raise e
+        finally:
+            cursor.close()
 
 
 
