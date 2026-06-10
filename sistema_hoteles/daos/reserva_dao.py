3d0cd85d7cd7b379b0f9dab257e58c55ad42eb22
leandro291 <lenadrito1@gmail.com>
2026-06-10 08:40:09 -0500

    feat: Agregar método para cambiar el estado de una reserva y obtener el total de la reserva

--- sistema_hoteles/daos/reserva_dao.py
+++ sistema_hoteles/daos/reserva_dao.py
@@ -22,3 +22,40 @@ class ReservaDAO(BaseDAO):
         )
 
         return self.insertar_y_retornar_id(consulta, valores)
+    
+    def cambiar_estado_reserva(self, id_reserva: int, nuevo_estado: str) -> bool:
+        
+        consulta = """
+            UPDATE reserva SET estado_reserva = %s WHERE idreserva = %s
+        """
+
+        valores = (nuevo_estado, id_reserva)
+
+        self.cambiar_estado(consulta, valores)
+    
+    def obtener_total_reserva(self, id_reserva: int) -> float:
+
+        consulta = """
+            SELECT total
+            FROM reserva
+            WHERE idreserva = %s
+        """
+
+        valores = (id_reserva,)
+
+        cursor = self.conexion.cursor()
+
+        try:
+
+            cursor.execute(consulta, valores)
+            resultado = cursor.fetchone()
+
+            if not resultado:
+                raise Exception("No se ha eonctrado valor para el ID ingresado")
+            
+            return float(resultado[0])
+
+        except Exception as e:
+            raise e
+        finally:
+            cursor.close()
