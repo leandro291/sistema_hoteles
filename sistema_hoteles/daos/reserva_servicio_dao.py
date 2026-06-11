9ffb04c7363a9aba1d71c6b9a63b4eac7e71bf9e
leandro291 <lenadrito1@gmail.com>
2026-06-11 06:18:17 -0500

    refactor: Actualizar consulta de inserción en ReservaServicioDAO para incluir precio_unitario y retornar id_reserva_servicio

--- sistema_hoteles/daos/reserva_servicio_dao.py
+++ sistema_hoteles/daos/reserva_servicio_dao.py
@@ -6,18 +6,20 @@ class ReservaServicioDAO(BaseDAO):
     def insertar_reserva_servicio(self, reserva_servicio: "ReservaServicio") -> bool:
 
         consulta = """
-            INSERT INTO reserva_servicio (idservicio, idreserva, cantidad, subtotal)
+            INSERT INTO reserva_servicio (id_servicio, id_reserva, precio_unitario, cantidad, subtotal)
             VALUES (%s, %s, %s, %s)
+            RETURNING id_reserva_servicio
         """
 
         valores = (
             reserva_servicio.id_servicio,
             reserva_servicio.id_reserva,
+            reserva_servicio.precio_unitario,
             reserva_servicio.cantidad,
             reserva_servicio.subtotal
         )
 
-        return self.insertar_sin_retorno_id(consulta, valores)
+        return self.insertar_datos(consulta, valores)
     
     def eliminar_reserva_servicio(self, id_servicio: int, id_reserva: int):
 
