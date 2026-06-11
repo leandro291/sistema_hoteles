f9fb584cc43ed518070eeb3bb8ce386eaa25f9d4
leandro291 <lenadrito1@gmail.com>
2026-06-10 22:15:01 -0500

    refactor: Renombrar atributos en el modelo Acompanante y ajustar el constructor

--- sistema_hoteles/models/acompanante.py
+++ sistema_hoteles/models/acompanante.py
@@ -3,18 +3,22 @@ from pydantic import BaseModel
 class AcompananteSchema(BaseModel):
     nombre: str
     apellido: str
-    dni: str
+    tipo_documento: str
+    numero_documento: str
     edad: int
 
 class Acompanante:
-    def __init__(self, id_reserva: int, nombre: str, apellido: str, dni: str, edad: int, telefono: str, id_acompanante: None = None):
+    def __init__(self, id_reserva_habitacion: int, nombre: str, apellido: str, telefono: str, 
+                 tipo_documento: str, numero_documento: str, id_acompanante: None = None):
         self.id_acompanante = id_acompanante
-        self.id_reserva = id_reserva
+
         self.nombre = nombre
         self.apellido = apellido
-        self.dni = dni
-        self.edad = edad
         self.telefono = telefono
+        self.tipo_documento = tipo_documento
+        self.numero_documento = numero_documento
+
+        self.id_reserva_habitacion = id_reserva_habitacion
 
 
 
