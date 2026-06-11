5e4a7436fa9f69a205eb7b06b7d3cc5e02d2836b
leandro291 <lenadrito1@gmail.com>
2026-06-10 22:16:07 -0500

    refactor: Eliminar atributos cantidad_huesped y total del constructor de Reserva

--- sistema_hoteles/models/reserva.py
+++ sistema_hoteles/models/reserva.py
@@ -7,20 +7,19 @@ class ReservaSchema(BaseModel):
     fecha_reserva: date
     fecha_entrada: date
     fecha_salida: date
-    cantidad_huesped: int
     estado_reserva: str
-    total: Decimal
-
 class Reserva:
-    def __init__(self, id_cliente: int, fecha_reserva: date, fecha_entrada: date, fecha_salida: date, cantidad_huesped: int, estado_reserva: str, total: Decimal, id_reserva: None=None):
+    def __init__(self, id_cliente: int, id_usuario: int, fecha_reserva: date, fecha_entrada: date, fecha_salida: date, 
+                 estado_reserva: str, id_reserva: None=None):
         self.id_reserva = id_reserva
-        self.id_cliente = id_cliente
+
         self.fecha_reserva = fecha_reserva
         self.fecha_entrada = fecha_entrada
         self.fecha_salida = fecha_salida
-        self.cantidad_huesped = cantidad_huesped
         self.estado_reserva = estado_reserva
-        self.total = total
+
+        self.id_usuario = id_usuario
+        self.id_cliente = id_cliente
 
 
         
