f577a154a28a99d37c8e1cb6adad5bd26bcfdb14
leandro291 <lenadrito1@gmail.com>
2026-06-11 06:19:02 -0500

    refactor: Renombrar método insertar_y_retornar_id a insertar_datos y ajustar la estructura de métodos en BaseDAO

--- sistema_hoteles/daos/base_dao.py
+++ sistema_hoteles/daos/base_dao.py
@@ -1,10 +1,10 @@
 from config.database import ConexionDB
 
 class BaseDAO:
-    def __init__(self, conexion : "ConexionDB"):
+    def __init__(self, conexion):
         self.conexion = conexion
 
-    def insertar_y_retornar_id(self, sql: str, valores: tuple) -> int:
+    def insertar_datos(self, sql: str, valores: tuple) -> int:
 
         cursor = self.conexion.cursor()
 
@@ -19,14 +19,40 @@ class BaseDAO:
         finally:
             if cursor:
                 cursor.close()
+    
+    def obtener_dato_por_id(self, sql: str, id: int) -> int:
+        pass
 
-    def insertar_sin_retorno_id(self, sql, valores: tuple):
+    def actualizar_dato_por_id(self, sql: str, id: int) -> None:
+        pass
+
+    def eliminar_dato_por_id(self, sql: str, id: int) -> None:
+        
+        cursor = self.conexion.cursor()
+
+        try:
+
+            cursor.execute(sql, id)
+            res = cursor.fetchone()
+
+            return res
+        
+        except Exception as e:
+            raise e
+        finally:
+            if cursor:
+                cursor.close()
+
+    def cambiar_estado(self, sql: str, valores: tuple) -> bool:
 
         cursor = self.conexion.cursor()
 
         try:
 
             cursor.execute(sql, valores)
+            if cursor.rowcount == 0:
+                raise Exception(f"La reserva ingresada no existe")
+                
             return True
 
         except Exception as e:
