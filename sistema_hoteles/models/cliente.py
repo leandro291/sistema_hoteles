9d45d8fc718a21a655e522b228ad0ff7cf7a2e4b
leandro291 <lenadrito1@gmail.com>
2026-06-10 22:15:14 -0500

    refactor: Renombrar atributo 'dni' a 'tipo_documento' y 'numero_documento' en el modelo ClienteSchema

--- sistema_hoteles/models/cliente.py
+++ sistema_hoteles/models/cliente.py
@@ -4,17 +4,21 @@ from pydantic import BaseModel
 class ClienteSchema(BaseModel):
     nombre: str
     apellido: str
-    dni: str
+    tipo_documento: str
+    numero_documento: str
     telefono: str
     correo: str
     direccion: str
 
 class Cliente:
-    def __init__(self, nombre: str, apellido: str, dni: str, telefono: str, correo: str, direccion: str, id_cliente: None = None):
+    def __init__(self, nombre: str, apellido: str, tipo_documento: str, numero_documento: str, 
+                 telefono: str, correo: str, direccion: str, id_cliente: None = None):
+        
         self.id_cliente = id_cliente
         self.nombre = nombre
         self.apellido = apellido
-        self.dni = dni
+        self.tipo_documento = tipo_documento
+        self.numero_documento = numero_documento
         self.telefono = telefono
         self.correo = correo
         self.direccion = direccion
