2011d6a44a1c78b79d00dab7d24fe4cc32e3351e
leandro291 <lenadrito1@gmail.com>
2026-06-11 06:15:15 -0500

    refactor: Eliminar atributo fecha_reserva del esquema de Reserva y ajustar constructor

--- sistema_hoteles/models/reserva.py
+++ sistema_hoteles/models/reserva.py
@@ -4,16 +4,14 @@ from decimal import Decimal
 from pydantic import BaseModel
 
 class ReservaSchema(BaseModel):
-    fecha_reserva: date
     fecha_entrada: date
     fecha_salida: date
-    estado_reserva: str
+
 class Reserva:
-    def __init__(self, id_cliente: int, id_usuario: int, fecha_reserva: date, fecha_entrada: date, fecha_salida: date, 
+    def __init__(self, id_cliente: int, id_usuario: int, fecha_entrada: date, fecha_salida: date, 
                  estado_reserva: str, id_reserva: None=None):
         self.id_reserva = id_reserva
-
-        self.fecha_reserva = fecha_reserva
+        
         self.fecha_entrada = fecha_entrada
         self.fecha_salida = fecha_salida
         self.estado_reserva = estado_reserva
