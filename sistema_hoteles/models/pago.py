7692ee19c8f34db780bbcc1fb06d1be29d8586f5
leandro291 <lenadrito1@gmail.com>
2026-06-10 22:15:45 -0500

    refactor: Agregar id_usuario al constructor de la clase Pago

--- sistema_hoteles/models/pago.py
+++ sistema_hoteles/models/pago.py
@@ -10,12 +10,15 @@ class PagoSchema(BaseModel):
     tipo_comprobante: str
 
 class Pago:
-    def __init__(self, id_reserva: int,  monto: Decimal, fecha_pago: date, metodo_pago: str, estado_pago: str, tipo_comprobante: str, id_pago: None = None):
+    def __init__(self, id_reserva: int, id_usuario: int, monto: Decimal, fecha_pago: date, metodo_pago: str, 
+                 estado_pago: str, tipo_comprobante: str, id_pago: None = None):
         self.id_pago = id_pago
+
         self.monto = monto
         self.fecha_pago = fecha_pago
         self.metodo_pago = metodo_pago
-        self.estado_pago = estado_pago
         self.tipo_comprobante = tipo_comprobante
+        self.estado_pago = estado_pago
 
         self.id_reserva = id_reserva
+        self.id_usuario = id_usuario
