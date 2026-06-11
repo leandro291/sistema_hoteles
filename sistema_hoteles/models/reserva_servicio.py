efdbb334186c6b7f574907057922c689ed5d921b
leandro291 <lenadrito1@gmail.com>
2026-06-10 22:16:30 -0500

    refactor: Agregar atributo precio_unitario al constructor de ReservaServicio

--- sistema_hoteles/models/reserva_servicio.py
+++ sistema_hoteles/models/reserva_servicio.py
@@ -2,13 +2,19 @@ from decimal import Decimal
 from pydantic import BaseModel
 
 class ReservaServicioSchema(BaseModel):
+    precio_unitario: Decimal
     cantidad: int
     subtotal: Decimal
 
 class ReservaServicio:
-    def __init__(self, id_servicio: int, id_reserva: int, cantidad: int, subtotal: Decimal):
-        self.id_servicio = id_servicio    
-        self.id_reserva = id_reserva      
+    def __init__(self, id_servicio: int, id_reserva: int, precio_unitario: Decimal, cantidad: int, 
+                 subtotal: Decimal, id_reserva_servicio: None = None):
+        self.id_reserva_servicio = id_reserva_servicio
+
+        self.precio_unitario = precio_unitario
         self.cantidad = cantidad
         self.subtotal = subtotal
+
+        self.id_servicio = id_servicio    
+        self.id_reserva = id_reserva      
 
