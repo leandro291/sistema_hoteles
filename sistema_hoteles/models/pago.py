c5440b80e1135039bd226caba621d9f575035ef9
leandro291 <lenadrito1@gmail.com>
2026-06-11 06:16:40 -0500

    refactor: Eliminar atributo fecha_pago del esquema de Pago y ajustar constructor

--- sistema_hoteles/models/pago.py
+++ sistema_hoteles/models/pago.py
@@ -4,18 +4,16 @@ from pydantic import BaseModel
 
 class PagoSchema(BaseModel):
     monto: Decimal
-    fecha_pago: date
     metodo_pago: str
     estado_pago: str
     tipo_comprobante: str
 
 class Pago:
-    def __init__(self, id_reserva: int, id_usuario: int, monto: Decimal, fecha_pago: date, metodo_pago: str, 
+    def __init__(self, id_reserva: int, id_usuario: int, monto: Decimal, metodo_pago: str, 
                  estado_pago: str, tipo_comprobante: str, id_pago: None = None):
         self.id_pago = id_pago
 
         self.monto = monto
-        self.fecha_pago = fecha_pago
         self.metodo_pago = metodo_pago
         self.tipo_comprobante = tipo_comprobante
         self.estado_pago = estado_pago
