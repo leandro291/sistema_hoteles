b20705b9bb54f115283844fb36c0b9781645e214
leandro291 <lenadrito1@gmail.com>
2026-06-12 15:24:35 -0500

    fix: Mejorar el manejo de errores y cerrar cursores en BaseDAO

--- sistema_hoteles/daos/base_dao.py
+++ sistema_hoteles/daos/base_dao.py
@@ -1,5 +1,3 @@
-from config.database import ConexionDB
-
 class BaseDAO:
     def __init__(self, conexion):
         self.conexion = conexion
@@ -11,37 +9,56 @@ class BaseDAO:
         try:
 
             cursor.execute(sql, valores)
+
+            self.conexion.commit()
             id_obtenido = cursor.fetchone()[0]
             return id_obtenido
-
+        
         except Exception as e:
-            raise e
+            self.conexion.rollback()
+            raise Exception(f"Ha ocurrido un error en la Base de Datos: {e}")
         finally:
             if cursor:
                 cursor.close()
     
     def obtener_dato_por_id(self, sql: str, id: int) -> int:
-        pass
 
-    def actualizar_dato_por_id(self, sql: str, id: int) -> None:
-        pass
-
-    def eliminar_dato_por_id(self, sql: str, id: int) -> None:
-        
         cursor = self.conexion.cursor()
 
         try:
 
             cursor.execute(sql, id)
+            self.conexion.commit()
+
             res = cursor.fetchone()
 
             return res
         
         except Exception as e:
-            raise e
+            self.conexion.rollback()
+            raise Exception(f"Ha ocurrido un error en la Base de Datos: {e}")
         finally:
             if cursor:
                 cursor.close()
+
+    def actualizar_dato_por_id(self, sql: str, id: int) -> None:
+        pass
+
+    def eliminar_dato_por_id(self, sql: str, id: int) -> None:
+        cursor = self.conexion.cursor()
+        try:
+
+            cursor.execute(sql, id)
+            self.conexion.commit()
+
+            if cursor.rowcount == 0:
+                raise Exception("No se encontró el registro para eliminar")
+
+        except Exception as e:
+            self.conexion.rollback()
+            raise Exception(f"Ha ocurrido un error en la Base de Datos: {e}")
+        finally:
+            cursor.close()
 
     def cambiar_estado(self, sql: str, valores: tuple) -> bool:
 
@@ -50,13 +67,16 @@ class BaseDAO:
         try:
 
             cursor.execute(sql, valores)
+            self.conexion.commit()
+            
             if cursor.rowcount == 0:
                 raise Exception(f"La reserva ingresada no existe")
                 
             return True
 
         except Exception as e:
-            raise e
+            self.conexion.rollback()
+            raise Exception(f"Ha ocurrido un error en la Base de Datos: {e}")
         finally:
             if cursor:
                 cursor.close()
