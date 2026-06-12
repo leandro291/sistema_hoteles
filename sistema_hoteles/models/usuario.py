fdc08f3e81ce146580273fb7cff597688e408a61
leandro291 <lenadrito1@gmail.com>
2026-06-12 15:22:06 -0500

    feat: Eliminar el campo 'apellido' de Usuario y UsuarioSchema

--- sistema_hoteles/models/usuario.py
+++ sistema_hoteles/models/usuario.py
@@ -2,14 +2,12 @@ from pydantic import BaseModel
 
 class UsuarioSchema(BaseModel):
     nombre: str
-    apellido: str
     contrasena: str
     rol: str
 
 class Usuario:
-    def __init__(self, nombre: str, apellido: str, contrasena: str, rol: str, id_usuario: None = None):
+    def __init__(self, nombre: str, contrasena: str, rol: str, id_usuario: None = None):
         self.id_usuario = id_usuario
         self.nombre = nombre
-        self.apellido = apellido
         self.contrasena = contrasena
         self.rol = rol
