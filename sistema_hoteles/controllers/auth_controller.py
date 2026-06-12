f2d2d11e4d152912c67682a8bed1e35295f3b73a
leandro291 <lenadrito1@gmail.com>
2026-06-12 15:24:56 -0500

    feat: Refactor el método registrar_nuevo_usuario para validar datos de entrada

--- sistema_hoteles/controllers/auth_controller.py
+++ sistema_hoteles/controllers/auth_controller.py
@@ -1,23 +1,37 @@
 from config.database import ConexionDB
-from models import Usuario
+from pydantic import ValidationError
 
+from models import Usuario, UsuarioSchema
 from daos.usuario_dao import UsuarioDAO
 
 class AuthController:
     def __init__(self):
         self.db = ConexionDB()
 
-    def registrar_nuevo_usuario(self, usuario: "Usuario") -> None:
+    def registrar_nuevo_usuario(self, nombre: str, contrasena: str, rol: str) -> None:
         
-        conexion = self.db.obtener_conexion()
+        try:
+            validador_usuario = UsuarioSchema(
+                nombre=nombre,
+                contrasena=contrasena,
+                rol=rol
+            )   
+        except ValidationError as e:
+            raise ValueError("Por favor, revisa que los datos ingreados sean correctos")
+        
+        usuario = Usuario(
+            nombre=validador_usuario.nombre,
+            contrasena=validador_usuario.contrasena,
+            rol=validador_usuario.rol
+        )
 
+        conexion = self.db.obtener_conexion()
         try:
 
             dao_usuario = UsuarioDAO(conexion)
-
             return dao_usuario.insertar_nuevo_usuario(usuario)
+        
+            
 
         except Exception as e:
             raise Exception(f"Ha ocurrido un error en la Base de Datos: {e}")
-        finally:
-            self.db.cerrar_conexion()
