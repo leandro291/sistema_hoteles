ad6f296d97b17c8717acd3d90510b865d631fd69
leandro291 <lenadrito1@gmail.com>
2026-06-13 15:46:57 -0500

    feat: Agregar campo descripción en el esquema y constructor de TipoHabitacion

--- sistema_hoteles/models/tipo_habitacion.py
+++ sistema_hoteles/models/tipo_habitacion.py
@@ -6,13 +6,16 @@ class TipoHabitacionSchema(BaseModel):
     nombre: str
     precio : Decimal
     capacidad: int
+    descripcion: str
 
 class TipoHabitacion:
-    def __init__(self, nombre: str, precio: Decimal, capacidad: int, id_tipo_habitacion: None = None):
+    def __init__(self, nombre: str, precio: Decimal, capacidad: int, descripcion: str, id_tipo_habitacion: None = None):
         self.id_tipo_habitacion = id_tipo_habitacion
+        
         self.nombre = nombre
         self.precio = precio
         self.capacidad = capacidad
+        self.descripcion = descripcion
 
 
 
