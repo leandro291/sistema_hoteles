a23ac58f02e2e661d06c4e580501e0ec7e5187a4
leandro291 <lenadrito1@gmail.com>
2026-06-14 16:15:30 -0500

    feat: Reorganizar importaciones y métodos de visualización en AppManager

--- sistema_hoteles/main.py
+++ sistema_hoteles/main.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from utils.utils import limpiar_ventana
 
-from view.dashboard_view import DashboardView
+from view.pago_view import PagoView
 from view.login_view import LoginView
+from view.cliente_view import ClienteView
+from view.dashboard_view import DashboardView
+from view.habitacion_view import HabitacionView
+from view.asignacion_view import AsignacionView
+from view.servicios_view import ServiciosView
 
 
 class AppManager:
@@ -20,10 +25,33 @@ class AppManager:
         limpiar_ventana(self.root)
         self.vista_actual = LoginView(self.root, manager=self)
 
-    def mostrar_dashboard(self, datos_usuario):
-        self.usuario_actual = datos_usuario
+    def mostrar_dashboard(self, datos_usuario=None):
+
+        if datos_usuario is not None:
+            self.usuario_actual = datos_usuario
+
         limpiar_ventana(self.root)
         self.vista_actual = DashboardView(self.root, manager=self)
+
+    def mostrar_habitaciones(self):
+        limpiar_ventana(self.root)
+        self.vista_actual = HabitacionView(self.root, manager=self)
+
+    def mostrar_asignaciones(self):
+        limpiar_ventana(self.root)
+        self.vista_actual = AsignacionView(self.root, manager=self)
+
+    def mostrar_clientes(self):
+        limpiar_ventana(self.root)
+        self.vista_actual = ClienteView(self.root, manager=self)
+
+    def mostrar_pagos(self):
+        limpiar_ventana(self.root)
+        self.vista_actual = PagoView(self.root, manager=self)
+    
+    def mostrar_servicios(self):
+        limpiar_ventana(self.root)
+        self.vista_actual = ServiciosView(self.root, manager=self)
 
 if __name__ == "__main__":
     ventana  = tk.Tk()
