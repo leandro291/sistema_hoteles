d4fbc9cec266181c67a6012289ff560d7521c1f5
leandro291 <lenadrito1@gmail.com>
2026-06-15 12:20:57 -0500

    feat: Agregar vistas de cliente, asignación y servicios al módulo de vistas

--- sistema_hoteles/view/__init__.py
+++ sistema_hoteles/view/__init__.py
@@ -1,2 +1,7 @@
+from .pago_view import PagoView
 from .login_view import LoginView
+from .cliente_view import ClienteView
 from .dashboard_view import DashboardView
+from .habitacion_view import HabitacionView
+from .asignacion_view import AsignacionView
+from .servicios_view import ServiciosView
