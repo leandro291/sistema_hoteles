4d264eb2b14fe0e9b1a20137105c57644427c245
leandro291 <lenadrito1@gmail.com>
2026-06-15 15:38:05 -0500

    fix: Añadir validación para el precio en ServicioSchema

--- sistema_hoteles/models/servicio.py
+++ sistema_hoteles/models/servicio.py
@@ -1,15 +1,17 @@
-from typing import List
 from decimal import Decimal
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ServicioSchema(BaseModel):
     nombre: str
     descripcion: str
-    precio: Decimal
+    precio: Decimal = Field(gt=0)
+
+    
 
 class Servicio:
     def __init__(self, nombre: str, descripcion: str, precio: Decimal, id_servicio: None = None):
         self.id_servicio = id_servicio
+        
         self.nombre = nombre
         self.descripcion = descripcion
         self.precio = precio
