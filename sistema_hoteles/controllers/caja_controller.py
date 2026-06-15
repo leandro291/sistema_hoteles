5bf76b073211211c29a4809bf7e59786698667b5
leandro291 <lenadrito1@gmail.com>
2026-06-15 15:37:23 -0500

    feat: Mejorar el registro de pagos y manejo de reservas en CajaController

--- sistema_hoteles/controllers/caja_controller.py
+++ sistema_hoteles/controllers/caja_controller.py
@@ -1,12 +1,12 @@
+from decimal import Decimal
+from pydantic import ValidationError
 from config.database import ConexionDB
-from datetime import datetime
+
 from models.pago import Pago, PagoSchema
 from daos.pago_dao import PagoDAO
 from daos.reserva_dao import ReservaDAO
 from daos.habitacion_dao import HabitacionDAO
-from daos.reserva_servicio_dao import ReservaServicioDAO
 
-from decimal import Decimal
 
 class CajaController:
     def __init__(self):
@@ -24,30 +24,45 @@ class CajaController:
         
     def registrar_pago_completo(self, id_reserva: int, monto: float, metodo: str, comprobante: str, id_usuario: int):
             
+        try:
+            validador_pago = PagoSchema(
+                monto=Decimal(str(monto)),
+                metodo_pago=metodo,
+                estado_pago="Completado",
+                tipo_comprobante=comprobante
+            )
+        except ValidationError as e:
+            raise ValueError(f"Ha ocurrido un error al registrar datos del pago: {e}")
+        
         conexion = self.db.obtener_conexion()
 
         try:
             dao_pago = PagoDAO(conexion)
+            dao_reserva = ReservaDAO(conexion)
+            dao_habitacion = HabitacionDAO(conexion)
             
             nuevo_pago = Pago(
                 id_reserva=id_reserva,
                 id_usuario=id_usuario,
-                monto=Decimal(str(monto)), 
-                metodo_pago=metodo,
-                estado_pago='Completado',  
-                tipo_comprobante=comprobante
+                monto=validador_pago.monto, 
+                metodo_pago=validador_pago.metodo_pago,
+                estado_pago=validador_pago.estado_pago,  
+                tipo_comprobante=validador_pago.tipo_comprobante
             )
             
-            dao_pago.registrar_transaccion_pago(nuevo_pago)
+            dao_pago.insertar_pago(nuevo_pago)
+            dao_reserva.cambiar_estado_reserva(id_reserva, "Finalizado")
+            dao_habitacion.cambiar_estado_por_reserva(id_reserva)
             
         except Exception as e:
-            raise Exception(f"Fallo en la caja registradora: {e}")
+            raise Exception(f"Ha ocurrido un error en la BD: {e}")
 
 
     def obtener_todos_los_pagos(self) -> list:
-        conexion = None
+
+        conexion = self.db.obtener_conexion()
+
         try:
-            conexion = self.db.obtener_conexion()
             dao_pago = PagoDAO(conexion) 
             
             registros = dao_pago.obtener_historial_pagos()
