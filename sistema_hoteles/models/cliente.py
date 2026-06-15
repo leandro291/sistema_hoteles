961bfc35c3e66d596d6fc8b0d2e560233d082248
leandro291 <lenadrito1@gmail.com>
2026-06-15 13:51:41 -0500

    feat: Cambiar el tipo de correo a EmailStr y agregar validaciones para teléfono, número de documento, nombre y apellido en ClienteSchema

--- sistema_hoteles/models/cliente.py
+++ sistema_hoteles/models/cliente.py
@@ -1,5 +1,4 @@
-from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, Field, EmailStr
 
 class ClienteSchema(BaseModel):
     nombre: str
@@ -7,8 +6,32 @@ class ClienteSchema(BaseModel):
     tipo_documento: str
     numero_documento: str
     telefono: str
-    correo: str
+    correo: EmailStr
     direccion: str
+
+    @field_validator('telefono')
+    @classmethod
+    def validate_telefono(cls, valor: str) -> str:
+
+        if len(valor) != 9 or not valor.isdigit():
+            raise ValueError("El teléfono debe tener exactamente 9 digitos")
+        
+        return valor
+    
+    @field_validator('numero_documento')
+    def validate_numero_documento(cls, valor: str) -> str:
+        if not valor.isdigit():
+            raise ValueError(f"El Numero de Documento {valor} debe estar formado de digitos")
+        
+        return valor
+    
+    @field_validator('nombre', 'apellido')
+    @classmethod
+    def validate_no_numeros(cls, valor: str) -> str:
+        if any(char.isdigit() for char in valor):
+            raise ValueError("El nombre y el apellido no deben contener numeros")
+            
+        return valor
 
 class Cliente:
     def __init__(self, nombre: str, apellido: str, tipo_documento: str, numero_documento: str, 
